146b210c9fd7a30d525b445d2f0d697d8efe5fab
jasmarine <33455193+jasmarine@users.noreply.github.com>
2019-06-20 15:35:55 +1000

    Create countdown.c

---
+++ countdown.c
@@ -0,0 +1,19 @@
+//Jasmine Al-Hassani z5258857
+//19/06/2019
+
+#include <stdio.h>
+
+int main(void) {
+
+    int count = 10;
+
+    while (count >= 0) {
+
+        printf("%d\n", count);
+
+        count--;
+
+    }
+
+    return 0;
+}
